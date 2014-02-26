07a17a0f98ce00fb5e1333e0bb30907d2ad75917
Christopher Smith <chrylis@gmail.com>
2014-02-26 04:52:15 -0600

    add ApiUrlParts for constant references to paths

---
+++ artsquare-gallery-api/src/main/java/com/artsquare/gallery/api/rest/ApiUrlParts.java
@@ -0,0 +1,15 @@
+package com.artsquare.gallery.api.rest;
+
+public final class ApiUrlParts {
+
+	public static final String ID_PATHVAR = "/{id}";
+	
+	public static final String PERSON_BASE = "/p";
+	public static final String PERSON = PERSON_BASE + ID_PATHVAR;
+	
+	public static final String WORK_BASE = "/w";
+	public static final String WORK = WORK_BASE + ID_PATHVAR;
+	
+	private ApiUrlParts() {
+	}
+}
