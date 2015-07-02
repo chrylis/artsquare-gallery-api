d7bfd16d64f78b004fd1a0bcea2ee265b0f76751
Christopher Smith <chrylis@gmail.com>
2015-07-02 13:56:02 -0500

    add "link ID" fields to WorkResource

--- src/main/java/com/artsquare/gallery/api/rest/v0/WorkResource.groovy
+++ src/main/java/com/artsquare/gallery/api/rest/v0/WorkResource.groovy
@@ -13,4 +13,35 @@ class WorkResource extends ResourceWithPicture {
     String description
 
     Integer year
+
+    String slug
+
+    Boolean visible
+
+    Boolean adult
+
+    /*
+     * The following are linked resources. For pragmatic reasons, they are represented
+     * inline by ID rather than link. This will be revisited, especially if Spring HATEOAS
+     * starts providing helpful methods for parsing URIs into domain specifiers.
+     */
+
+    String artist
+
+    String mainImage
+
+    void setMainImage(String mainImage) {
+        this.mainImage = mainImage
+
+        if(!allImages.contains(mainImage)) {
+            allImages << mainImage
+        }
+    }
+
+    List<String> allImages
+
+    @Override
+    String toString() {
+        "Work[$title by $artist]"
+    }
 }
