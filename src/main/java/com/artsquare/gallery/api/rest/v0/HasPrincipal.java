c051e5ca8d5c4c384783e0de49f85f956bf536db
Christopher Smith <chrylis@gmail.com>
2015-09-24 11:11:28 -0500

    add HasPrincipal interface

---
+++ src/main/java/com/artsquare/gallery/api/rest/v0/HasPrincipal.java
@@ -0,0 +1,5 @@
+package com.artsquare.gallery.api.rest.v0;
+
+public interface HasPrincipal {
+    String principal();
+}
--- src/main/java/com/artsquare/gallery/api/rest/v0/digitization/OnSiteBookingResource.groovy
+++ src/main/java/com/artsquare/gallery/api/rest/v0/digitization/OnSiteBookingResource.groovy
@@ -3,21 +3,24 @@ package com.artsquare.gallery.api.rest.v0.digitization
 import groovy.transform.CompileStatic
 import groovy.transform.ToString
 
+import com.artsquare.gallery.api.rest.v0.HasPrincipal
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 
 @JsonInclude(Include.NON_NULL)
 @CompileStatic
 @ToString
-class OnSiteBookingResource {
+class OnSiteBookingResource implements HasPrincipal {
 
     String artist
+    
+    final String principal() { artist }
 
     String service
 
-    Map<Pricing.PerWork, Integer> worksBooked
+    Map<Pricing.PerWork, Integer> worksBooked = new EnumMap(Pricing.PerWork)
 
-    Set<Pricing.PerSession> options
+    Set<Pricing.PerSession> options = EnumSet.noneOf(Pricing.PerSession)
 
     Date requestedTime
     
